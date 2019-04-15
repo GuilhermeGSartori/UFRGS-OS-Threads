56bec19d01bdf990c75e90e62d002f97f8fc8235
Renan Kummer <35353992+RenanKummer@users.noreply.github.com>
2019-04-15 02:13:45 -0300

    Add Plug N' Play CUnit Framework files

---
+++ testes/include/PnP-CUnit/pnpcunit_Bool.h
@@ -0,0 +1,16 @@
+/**
+ * Definition of Boolean type.
+ * 
+ * @author Renan Kummer
+ */
+#ifndef PNPCUNIT_INCLUDE_BOOL_H
+#define PNPCUNIT_INCLUDE_BOOL_H
+
+typedef enum
+{
+    pnpcunit_FALSE,
+    pnpcunit_TRUE
+} pnpcunit_Bool;
+
+
+#endif // PNPCUNIT_INCLUDE_BOOL_H
