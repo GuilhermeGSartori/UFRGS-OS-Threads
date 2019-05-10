6077ce0c3be9c77e0505f6be5eb3f7f28e5e5460
Renan Kummer <35353992+RenanKummer@users.noreply.github.com>
2019-05-09 22:54:25 -0300

    Adjust tests

--- testes/src/main.c
+++ testes/src/main.c
@@ -58,7 +58,35 @@ void* test_CWait_Function3()
 	return NULL;
 }
 
+void test_CWait_Explicit()
+{
+	ccreate(test_CWait_Function1, NULL, HIGH);
+	ccreate(test_CWait_Function2, NULL, MEDIUM);
+	ccreate(test_CWait_Function3, NULL, LOW);
+
+    cwait(semaphore1);
+	cwait(semaphore1);
+	cwait(semaphore1);
+
+	if (!pnpcunit_assert_equal_int(-2, semaphore1->count))
+		has_passed = pnpcunit_FALSE;
+
+	FirstFila2(semaphore1->fila);
+	if (!pnpcunit_assert_equal_int(CTHREAD_SUCCESS, NextFila2(semaphore1->fila)))
+		has_passed = pnpcunit_FALSE;
+
+	if (has_passed)
+        printf("PASS!\n");
+    else
+        printf("FAIL!\n\n");
+}
+
 int main(int argc, char** argv)
 {
+    printf("\nTesting CWait:\n\n");
+    test_CWait_Explicit();
+
     TOGGLE_PNPCUNIT(configure_pnpcunit)
+
+
 }
