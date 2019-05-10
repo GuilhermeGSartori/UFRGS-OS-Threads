e4c06da85577a6f56b210a4cb6ca6d2d8ebf23f8
Renan Kummer <35353992+RenanKummer@users.noreply.github.com>
2019-05-09 23:10:39 -0300

    Adjust tests

--- testes/src/main.c
+++ testes/src/main.c
@@ -26,11 +26,11 @@
  */
 void* test_CWait_Function1()
 {
-	if (!pnpcunit_assert_equal_int(-1, semaphore1->count))
-		has_passed = pnpcunit_FALSE;
+	//if (!pnpcunit_assert_equal_int(-1, semaphore1->count))
+	//	has_passed = pnpcunit_FALSE;
 
-	if (!pnpcunit_assert_equal_int(CTHREAD_SUCCESS, FirstFila2(semaphore1->fila)))
-		has_passed = pnpcunit_FALSE;
+	//if (!pnpcunit_assert_equal_int(CTHREAD_SUCCESS, FirstFila2(semaphore1->fila)))
+	//	has_passed = pnpcunit_FALSE;
 
 	//cwait(semaphore1);
 
@@ -64,8 +64,8 @@ void test_CWait_Explicit()
 {
     semaphore1 = (csem_t*) malloc(sizeof(csem_t));
     semaphore2 = (csem_t*) malloc(sizeof(csem_t));
-    csem_init(semaphore1, 2);
-    csem_init(semaphore2, 2);
+    //csem_init(semaphore1, 2);
+    //csem_init(semaphore2, 2);
 
     test_CWait_Function1();
 	//ccreate(&test_CWait_Function1, NULL, HIGH);
@@ -76,17 +76,17 @@ void test_CWait_Explicit()
 	//cwait(semaphore1);
 	//cwait(semaphore1);
 
-	if (!pnpcunit_assert_equal_int(-2, semaphore1->count))
-		has_passed = pnpcunit_FALSE;
+	//if (!pnpcunit_assert_equal_int(-2, semaphore1->count))
+	//	has_passed = pnpcunit_FALSE;
 
-	FirstFila2(semaphore1->fila);
-	if (!pnpcunit_assert_equal_int(CTHREAD_SUCCESS, NextFila2(semaphore1->fila)))
-		has_passed = pnpcunit_FALSE;
+	//FirstFila2(semaphore1->fila);
+	//if (!pnpcunit_assert_equal_int(CTHREAD_SUCCESS, NextFila2(semaphore1->fila)))
+	//	has_passed = pnpcunit_FALSE;
 
-	if (has_passed)
-        printf("PASS!\n");
-    else
-        printf("FAIL!\n\n");
+	//if (has_passed)
+      //  printf("PASS!\n");
+    //else
+      //  printf("FAIL!\n\n");
 }
 
 int main(int argc, char** argv)
